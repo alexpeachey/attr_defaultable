61e130a39e07832e4ac92344cd1c0e837dd768db
alexpeachey <alex.peachey@gmail.com>
2014-04-23 16:33:58 -0700

    Fix closure problem

--- lib/attr_defaultable.rb
+++ lib/attr_defaultable.rb
@@ -3,7 +3,7 @@ require "attr_defaultable/version"
 module AttrDefaultable
   def attr_defaultable(att, default)
     define_method(att.to_sym) {
-      instance_variable_get("@#{att}".to_sym) || instance_variable_set("@#{att}".to_sym, default.call)
+      instance_variable_get("@#{att}".to_sym) || instance_variable_set("@#{att}".to_sym, instance_exec(&default))
     }
 
     define_method("#{att}=".to_sym) { |val|
